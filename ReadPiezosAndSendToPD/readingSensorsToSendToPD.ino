2dfdeb2e80473eab75848a27a33d9a1c06374f4d
Andrea Corcuera <andreacorcueram@gmail.com>
2016-03-16 17:44:16 +0100

    Read piezos and get the values in PD. Code for arduino and PD

---
+++ ReadPiezosAndSendToPD/readingSensorsToSendToPD.ino
@@ -0,0 +1,25 @@
+const int NUM_OF_KEYS = 2;
+const int THRESHOLD = 5;
+const int analogInputPins[NUM_OF_KEYS] = {A0, A1};
+
+byte arrPiezoVal[2*NUM_OF_KEYS+1];
+int piezoVal[NUM_OF_KEYS] = {0};
+int bufferlength = 0;
+char stringBuffer [50];
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+arrPiezoVal[0]= 0xc0;
+int index = 1;
+ for (int count; count < NUM_OF_KEYS; count++)
+    {
+        piezoVal[count] = analogRead(analogInputPins[count]);
+        arrPiezoVal[index++] = piezoVal[count] & 0x007f;
+        arrPiezoVal[index++] = piezoVal[count] >> 7;
+    }
+    Serial.write(arrPiezoVal, sizeof(arrPiezoVal));
+    delay(100);
+}
